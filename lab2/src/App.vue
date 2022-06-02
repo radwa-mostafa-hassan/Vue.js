24435249248c0a809519d504b797686fc61ce49b
Radwa Mostafa <radwamostafa922@gmail.com>
2022-06-02 22:12:52 +0200

    lab2

---
+++ lab2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<navigationApp/>
+<router-view></router-view>
+</div>
+</template>
+
+<script>
+import navigationApp from './components/navigation.vue'
+export default {
+  name: 'App',
+  components: {
+        navigationApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
